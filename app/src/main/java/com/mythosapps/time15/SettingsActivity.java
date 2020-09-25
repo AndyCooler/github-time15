9b1f37a22fbb15ccccbab1191ad94d3c5a378a5f
Andreas <andreas.kohler.76@gmail.com>
2020-09-25 22:28:29 +0200

    2.8 first SettingsActivity now with Toolbar and back navigation

--- app/src/main/java/com/mythosapps/time15/SettingsActivity.java
+++ app/src/main/java/com/mythosapps/time15/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.mythosapps.time15;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,5 +26,34 @@ public class SettingsActivity extends AppCompatActivity {
         //settingsFragment. ...
         getSupportFragmentManager().beginTransaction().add(R.id.settingsFrame, settingsFragment).commit();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_day) {
+            startMainActivity("");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startMainActivity(String withId) {
+        Intent intent = new Intent(this, MainActivity.class);
+        //intent.putExtra(EXTRA_MESSAGE, withId);
+        startActivity(intent);
     }
 }
