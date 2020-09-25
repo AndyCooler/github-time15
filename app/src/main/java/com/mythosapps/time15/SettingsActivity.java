b61f3903e1f8a44e70ed5c8d96c7811395692253
Andreas <andreas.kohler.76@gmail.com>
2020-09-25 21:59:42 +0200

    2.8 first SettingsActivity now with Toolbar

--- app/src/main/java/com/mythosapps/time15/SettingsActivity.java
+++ app/src/main/java/com/mythosapps/time15/SettingsActivity.java
@@ -3,32 +3,25 @@ package com.mythosapps.time15;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarSettings);
+        setSupportActionBar(toolbar);
+
 
         //If you want to insert data in your settings
         MainSettingsFragment settingsFragment = new MainSettingsFragment();
+        settingsFragment.setHasOptionsMenu(true);
         //settingsFragment. ...
-        getSupportFragmentManager().beginTransaction().replace(R.id.settingsFrame, settingsFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.settingsFrame, settingsFragment).commit();
 
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    private void setupActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
     }
 }
