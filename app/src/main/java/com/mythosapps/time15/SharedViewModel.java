889d08cf35962030156f6b3c7ecd5257146c4051
Andreas <andreas.kohler.76@gmail.com>
2020-12-12 23:04:27 +0100

    SharedViewModel initial value prevents crash

--- app/src/main/java/com/mythosapps/time15/SharedViewModel.java
+++ app/src/main/java/com/mythosapps/time15/SharedViewModel.java
@@ -9,7 +9,7 @@ import com.mythosapps.time15.types.KindOfDay;
 public class SharedViewModel extends ViewModel {
 
     // selected / current kind of day in MainActivity
-    MutableLiveData<KindOfDay> kindOfDay;
+    MutableLiveData<KindOfDay> kindOfDay = new MutableLiveData<>(KindOfDay.WORKDAY);
 
     public void selectKindOfDay(KindOfDay item) {
         kindOfDay.setValue(item);
