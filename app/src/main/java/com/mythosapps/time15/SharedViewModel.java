587106714da3ebfc9a4b98a8d033edfe231e1971
Andreas <andreas.kohler.76@gmail.com>
2020-12-13 23:27:17 +0100

    SharedViewModel idea

--- app/src/main/java/com/mythosapps/time15/SharedViewModel.java
+++ app/src/main/java/com/mythosapps/time15/SharedViewModel.java
@@ -6,6 +6,12 @@ import androidx.lifecycle.ViewModel;
 
 import com.mythosapps.time15.types.KindOfDay;
 
+/**
+ * ViewModel saves the state of the day's data presented in MainActivity as well as in other
+ * dialogs that mostly require only the ID. As it's live data linked to UI elements,
+ * this eliminates the need for complex modelToView / viewToModel methods. UI elements listen
+ * for changes which are represented on the UI and saved to the data store immediately.
+ */
 public class SharedViewModel extends ViewModel {
 
     // selected / current kind of day in MainActivity
