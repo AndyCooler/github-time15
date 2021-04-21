ecc025e9f699bdcde83ac1ddd0e38b5001d948f3
Andreas <andreas.kohler.76@gmail.com>
2021-04-21 22:58:37 +0200

    2.9.1 enter vacation in MonthOverviewActivity / date format

--- app/src/main/java/com/mythosapps/time15/util/DateInputTextWatcher.java
+++ app/src/main/java/com/mythosapps/time15/util/DateInputTextWatcher.java
@@ -57,7 +57,7 @@ public class DateInputTextWatcher implements TextWatcher {
                 clean = String.format("%02d%02d%02d", day, mon, year);
             }
 
-            clean = String.format("%s/%s/%s", clean.substring(0, 2),
+            clean = String.format("%s.%s.%s", clean.substring(0, 2),
                     clean.substring(2, 4),
                     clean.substring(4, 8));
 
