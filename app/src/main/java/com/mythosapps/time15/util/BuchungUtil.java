996caabde4688e6bf44adf5338969bfb9445bba1
Andreas <andreas.kohler.76@gmail.com>
2023-10-25 23:16:18 +0200

    40 bugfix in BuchungUtil

--- app/src/main/java/com/mythosapps/time15/util/BuchungUtil.java
+++ app/src/main/java/com/mythosapps/time15/util/BuchungUtil.java
@@ -1,5 +1,7 @@
 package com.mythosapps.time15.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Currency;
 import java.util.Locale;
@@ -11,17 +13,25 @@ public final class BuchungUtil {
 
     public static String getNettoForDisplay(double rate, int billableMinutes) {
         form.setCurrency(Currency.getInstance(currency));
-        return form.format(rate * billableMinutes / 60);
+        return form.format(round(rate * billableMinutes / 60, 2));
     }
 
     public static String getTaxForDisplay(double rate, int billableMinutes) {
         form.setCurrency(Currency.getInstance(currency));
-        return form.format(0.19 * rate * billableMinutes / 60);
+        return form.format(round(19 * rate * billableMinutes / 60 / 100, 2));
     }
 
     public static String getBruttoForDisplay(double rate, int billableMinutes) {
         form.setCurrency(Currency.getInstance(currency));
-        return form.format(1.19 * rate * billableMinutes / 60);
+        return form.format(round(119 * rate * billableMinutes / 60 / 100, 2));
+    }
+
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
 }
--- app/src/test/java/types/BuchungUtilTest.java
+++ app/src/test/java/types/BuchungUtilTest.java
@@ -12,9 +12,23 @@ import org.junit.Test;
 public class BuchungUtilTest {
 
     @Test
-    public void testBruttoNettoTax() {
-
+    public void testBruttoNettoTax1() {
         assertEquals(testValues("85.000,00", "16.150,00", "101.150,00"), bruttoNettoTax(85.0, 1000));
+    }
+
+    @Test
+    public void testBruttoNettoTax2() {
+        assertEquals(testValues("12.707,50", "2.414,43", "15.121,93"), bruttoNettoTax(85.0, 149.5));
+    }
+
+    @Test
+    public void testBruttoNettoTax3() {
+        assertEquals(testValues("10.433,75", "1.982,41", "12.416,16"), bruttoNettoTax(85.0, 122.75));
+    }
+
+    @Test
+    public void testBruttoNettoTax4() {
+        assertEquals(testValues("10.072,50", "1.913,78", "11.986,28"), bruttoNettoTax(85.0, 118.5));
     }
 
     private String[] testValues(String netto, String tax, String brutto) {
