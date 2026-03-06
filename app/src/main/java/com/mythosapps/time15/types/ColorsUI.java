a401f9e66102e19accbae81fd309b10ea1b5de9d
Andreas <andreas.kohler.76@gmail.com>
2026-03-06 22:16:52 +0100

    50 dark mode support w gradient

--- app/src/main/java/com/mythosapps/time15/types/ColorsUI.java
+++ app/src/main/java/com/mythosapps/time15/types/ColorsUI.java
@@ -39,7 +39,7 @@ public class ColorsUI {
     public static final Map<Integer, Integer> colorToChoice = new HashMap<>();
     // holo dark orange:
     public static final int DARK_ORANGE = Color.rgb(255, 255, 136); //#ffff8800
-    public static final int PURPLE = Color.rgb(128, 10, 128); //#800080
+    public static final int PURPLE = Color.rgb(106, 61, 232); //#6A3DE8
 
     static {
         choiceToColor.put(0, ColorsUI.DARK_BLUE_DEFAULT);
